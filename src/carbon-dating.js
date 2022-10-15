ce7aaeea8770899c9754488752c781a5d2c3a035
OLADUSHEK <yg1995@bk.ru>
2022-10-15 14:20:51 +0300

    task 2-4

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  let N = Number(sampleActivity);
+
+  if ( typeof(sampleActivity) === 'string' && !isNaN(sampleActivity) && N > 0 && N <= MODERN_ACTIVITY ) {
+    let k = Math.LN2 / HALF_LIFE_PERIOD;
+
+    return Math.ceil( Math.log(MODERN_ACTIVITY / N) / k );
+  }
+
+  return false;
 }
 
 module.exports = {
